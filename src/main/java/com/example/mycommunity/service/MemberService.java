1cc2acf09e38c41dc788490edc924b2f84a6a9bf
taemin <myloopmy@gmail.com>
2023-01-08 18:16:34 +0900

    회원중복조회 및 회원가입 , 전체 회원 조회 로직 설계

--- src/main/java/com/example/mycommunity/service/MemberService.java
+++ src/main/java/com/example/mycommunity/service/MemberService.java
@@ -4,6 +4,7 @@ import com.example.mycommunity.domain.Member;
 import com.example.mycommunity.repository.MemberRepository;
 import com.example.mycommunity.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -11,16 +12,28 @@ public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
 
-    //    회원가입
+    //회원가입
     public Long join(Member member){
+        //같은 이름이 있는 중복회원 X
+        validateDuplicateMember(member);//중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
 
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+    }
 
-        memberRepository.save(member);
-        return member.getId();
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 
 }
