3cebf82c2b61c56cda902d2ff08184003a74c9d6
taemin <myloopmy@gmail.com>
2023-01-09 23:01:23 +0900

    JPA 설계(완)

---
+++ src/main/java/com/example/mycommunity/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.mycommunity.repository;
+
+import com.example.mycommunity.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
